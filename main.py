90c29eda65d6ea1167dceab7136e4c1dadfce2f1
Egniel <myethero@yandex.ru>
2018-02-07 17:26:45 +0300

    reworked

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 import telebot
 from telebot import types
 from settings import TOKEN
@@ -7,28 +9,43 @@ from parse_horo import *
 
 bot = telebot.TeleBot(TOKEN)
 
-interval_markup = types.ReplyKeyboardMarkup(row_width=3)
+SIGN = ''
+
+ZODIAC_SIGNS = {
+    'Овен': 'Aries',
+    'Телец': 'Taurus',
+    'Близнецы': 'Gemini',
+    'Рак': 'Cancer',
+    'Лев': 'Leo',
+    'Дева': 'Virgo',
+    "Весы": 'Libra',
+    "Скорпион": 'Scorpio',
+    "Стрелец": 'Sagittarius',
+    'Козерог': 'Capricorn',
+    'Водолей': 'Aquarius',
+    'Рыбы': 'Pisces',
+}
+weekdays = {
+    'Сегодня': 'today',
+    'Вчера': 'yesterday',
+    'Завтра': 'tomorrow',
+    'Неделя': 'week',
+    'Год': 'year',
+}
+interval_markup = types.ReplyKeyboardMarkup(row_width=2)
 m_button1 = types.KeyboardButton('Вчера')
 m_button2 = types.KeyboardButton('Сегодня')
 m_button3 = types.KeyboardButton('Завтра')
 m_button4 = types.KeyboardButton('Неделя')
 m_button5 = types.KeyboardButton('Год')
+m_button6 = types.KeyboardButton('Выбрать другой знак')
 interval_markup.add(
-    m_button1, m_button2, m_button3, m_button4, m_button5)
-ZODIAC_SIGNS = (
-    'Овен',
-    'Телец',
-    'Близнецы',
-    'Рак',
-    'Лев',
-    'Дева',
-    "Весы",
-    "Скорпион",
-    "Стрелец",
-)
+    m_button1, m_button2, m_button3, m_button4, m_button5, m_button6)
 
 
 signs_markup = types.ReplyKeyboardMarkup(row_width=3)
+# for button in ZODIAC_SIGNS.keys():
+#     signs_markup.add(types.KeyboardButton(button))
 m_button1 = types.KeyboardButton('Овен')
 m_button2 = types.KeyboardButton('Телец')
 m_button3 = types.KeyboardButton('Близнецы')
@@ -48,16 +65,36 @@ signs_markup.add(
 
 @bot.message_handler(commands=['start', 'help'])
 def welcome(message):
-    bot.reply_to(message, message.text, reply_markup=signs_markup)
+    bot.send_message(
+        message.chat.id,
+        'Здравствуй, выбери знак зодиака',
+        reply_markup=signs_markup)
+
+
+
 
 
 @bot.message_handler()
 def text_handler(message):
-    if message.text in ZODIAC_SIGNS:
+    time.sleep(0.8)
+    global SIGN
+
+    if message.text in ZODIAC_SIGNS.keys():
+        SIGN = message.text
+        print(SIGN)
         bot.send_message(
             message.chat.id,
             'На какое время тебе нужен гороскоп?',
             reply_markup=interval_markup)
+    if message.text == 'Выбрать другой знак':
+        bot.send_message(
+            message.chat.id,
+            'Выбери знак зодиака',
+            reply_markup=signs_markup)
+    if message.text in weekdays.keys():
+        bot.send_message(
+            message.chat.id, get_horoscope(
+                ZODIAC_SIGNS.get(SIGN).lower(), weekdays.get(message.text)))
 
 
 bot.polling()
--- parse_horo.py
+++ parse_horo.py
@@ -7,7 +7,9 @@ def get_horoscope(sign, interval):
     """sign : string contains lowercase zodiac sign ex. capricon
     interval : yesterday, today, tomorrow, week, year
     """
-    mail_url = 'https://horo.mail.ru/prediction/{sign}/{interval}/'
-    r = requests.get(mail_url.format(sign=sign, interval=interval))
+    mail_url = 'https://horo.mail.ru/prediction/{sign}/{interval}/'.format(
+        sign=sign, interval=interval)
+    print(mail_url)
+    r = requests.get(mail_url)
     soup = BeautifulSoup(r.content, 'html.parser')
     return soup.select_one('.article__text').text.strip()
